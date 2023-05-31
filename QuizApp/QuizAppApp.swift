d73a95bb68e7cadbf615c04e6b648fdb5507bd5f
Purvesh Dodiya <purvesh.d@simformsolutions.com>
2023-05-31 19:25:38 +0530

    Initial commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Purvesh Dodiya on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
